0312df8a0ed43103e2b3525133d63b823bdff735
Ben Baker-Smith <bbakersmith@gmail.com>
2020-02-03 22:30:07 -0500

    foundation WIP

--- models/vhs_doorstop.py
+++ models/vhs_doorstop.py
@@ -4,35 +4,6 @@ from typing import List
 from shared.main import *
 
 
-# class Finger:
-#     @property
-#     def x(self) -> int:
-#         return self._x
-#
-#     @property
-#     def y(self) -> int:
-#         return self._y
-#
-#     @property
-#     def length(self) -> int:
-#         return self._length
-#
-#     @property
-#     def width(self) -> int:
-#         return self._width
-#
-#     @property
-#     def height(self) -> int:
-#         return self._height
-#
-#     def __init__(self, pos: Tuple[int, int], length: int, width: int, height: int):
-#         self._x = pos[0]
-#         self._y = pos[1]
-#         self._length = length
-#         self._width = width
-#         self._height = height
-
-
 #       ___
 #       | |
 # ____  |B|
@@ -43,13 +14,13 @@ from shared.main import *
 
 
 case_thickness = 2
-max_depth = 4
+max_depth = 3
 finger_space = 1
 door_height = 3.5
 
 roof_height = 4
 roof_diameter = 20
-roof_offset = [-3, 0]
+roof_offset = [-4, -2.25]
 
 skirt_size = 4
 skirt_height = 0.5
@@ -57,17 +28,17 @@ skirt_height = 0.5
 a_pos = [0, 5]
 a_length = 4.25
 a_width = 6.25
-a_depth = 3.5
+a_depth = 2.5
 
 b_pos = [8.5, 9.25]
 b_length = 3.5
 b_width = 5.25
-b_depth = 1.5
+b_depth = 2.5
 
 c_pos = [7.6, 2]
 c_length = 5
 c_width = 4.5
-c_depth = 1.5
+c_depth = 2.5
 
 
 def _finger(_pos: List[int], _length: int, _width: int, _depth: int):
@@ -85,21 +56,21 @@ def _finger(_pos: List[int], _length: int, _width: int, _depth: int):
     )
 
 
-def _skirt(_pos: List[int], _length: int, _width: int):
-    mod = skirt_size - finger_space
-
-    length = _length + mod
-    width = _width + mod
-    height = skirt_height
-
-    x = _pos[0] - (mod / 2)
-    y = _pos[1] - (mod / 2)
-    z = max_depth
-
-    return pipe(
-        cube([length, width, height]),
-        translate([x, y, z]),
-    )
+# def _skirt(_pos: List[int], _length: int, _width: int):
+#     mod = skirt_size - finger_space
+#
+#     length = _length + mod
+#     width = _width + mod
+#     height = skirt_height
+#
+#     x = _pos[0] - (mod / 2)
+#     y = _pos[1] - (mod / 2)
+#     z = max_depth
+#
+#     return pipe(
+#         cube([length, width, height]),
+#         translate([x, y, z]),
+#     )
 
 
 def _roof():
@@ -115,17 +86,31 @@ def _roof():
     )
 
 
+def _foundation():
+    length = b_length + b_pos[0] - finger_space
+    width = a_pos[1] + a_width - c_pos[1] - finger_space
+
+    x = finger_space / 2
+    y = c_pos[1] + (finger_space / 2)
+
+    return pipe(
+        cube([length, width, door_height]),
+        translate([x, y, max_depth])
+    )
+
 
 def main():
     seg_a = _finger(a_pos, a_length, a_width, a_depth)
     seg_b = _finger(b_pos, b_length, b_width, b_depth)
     seg_c = _finger(c_pos, c_length, c_width, c_depth)
 
-    skirt_b = _skirt(b_pos, b_length, b_width)
+    # skirt_b = _skirt(b_pos, b_length, b_width)
 
     roof = _roof()
 
-    final = seg_a + seg_b + seg_c + roof
+    foundation = _foundation()
+
+    final = seg_a + seg_b + seg_c + roof + foundation
 
     scad_render_to_file(final, "build/vhs_doorstop.scad")
 

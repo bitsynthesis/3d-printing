90e23572832f1e0db780d3d67aabb0f77dc953db
Ben Baker-Smith <bbakersmith@gmail.com>
2020-02-16 21:03:46 -0500

    updated hl3_headwater_faceplate

--- models/hl3_headwater_faceplate.py
+++ models/hl3_headwater_faceplate.py
@@ -2,101 +2,93 @@ from solid import *
 from shared.main import *
 
 
-# configurable values
+padding = 1.5
 
-
-padding = 2
 
 screw_size = 2.5
 screw_head_size = 5
 screw_head_height = 2.5
 screw_resolution = 30
 screw_dist = 2.5 + padding
-post_height = 5.2
-post_size = screw_size + 3.5
+
+
+post_height = 6.2
+post_size = screw_head_size + 2
+
 
 face_length = 58 + (padding * 2)
 face_width = 32 + (padding * 2)
 face_height = screw_head_height + 1
 
+
 window_length = 32
 window_width = 14
-
+window_dugout_length = 45
+window_dugout_width = 26.5
 window_x = (face_length / 2) - (window_length / 2)
 window_y = (face_width / 2) - (window_width / 2)
 
-resolution = 30
+
+screw_pos1 = [screw_dist, screw_dist];
+screw_pos2 = [screw_dist, face_width - screw_dist];
+screw_pos3 = [face_length - screw_dist, face_width - screw_dist];
+screw_pos4 = [face_length - screw_dist, screw_dist];
 
 
-# calculated values
-
-
-screw_pos_1 = [screw_dist, screw_dist]
-screw_pos_2 = [screw_dist, face_width - screw_dist]
-screw_pos_3 = [face_length - screw_dist, face_width - screw_dist]
-screw_pos_4 = [face_length - screw_dist, screw_dist]
-
-
-def _screw_head_hole():
-    return cylinder(
-        h=screw_head_height,
-        d=screw_head_size,
-        segments=resolution
+def screw_hole(x, y):
+    shaft_hole = cylinder(
+        h=face_height + post_height, d=screw_size, segments=screw_resolution
     )
 
-
-def _screw_shaft_hole():
-    return cylinder(
-        h=face_height + post_height,
-        d=screw_size,
-        segments=resolution
+    head_hole = cylinder(
+        h=screw_head_height, d=screw_head_size, segments=screw_resolution
     )
 
+    return pipe(shaft_hole + head_hole, hole(), translate([x, y, 0]))
 
-def _screw_hole(position):
+
+def mount_post(x, y):
     return pipe(
-        _screw_shaft_hole() + _screw_head_hole(),
-        translate([position[0], position[1], 0])
+        cylinder(h=post_height, d=post_size, segments=screw_resolution),
+        translate([x, y, face_height])
     )
 
 
-def _lcd_mount_post(position):
-    post = pipe(
-        cylinder(
-            h=post_height,
-            d=post_size,
-            segments=resolution
-        ),
-        translate([position[0], position[1], face_height])
+# screen is 8.65mm high
+
+
+def window():
+    cutout = cube([window_length, window_width, face_height])
+    dugout = pipe(
+        cube([window_dugout_length, window_dugout_width, face_height - 1]),
+        translate([
+            (window_length - window_dugout_length) / 2,
+            (window_width - window_dugout_width) / 2,
+            1
+        ])
     )
-    return post - _screw_hole(position)
 
-
-def _lcd_window():
     return pipe(
-        cube([window_length, window_width, face_height]),
+        cutout + dugout,
+        hole(),
         translate([window_x, window_y, 0])
     )
 
 
-def _body():
-    return cube([face_length, face_width, face_height])
-
-
 def main():
-    lcd_mount_1 = _lcd_mount_post(screw_pos_1)
-    lcd_mount_2 = _lcd_mount_post(screw_pos_2)
-    lcd_mount_3 = _lcd_mount_post(screw_pos_3)
-    lcd_mount_4 = _lcd_mount_post(screw_pos_4)
+    base = cube([face_length, face_width, face_height])
 
-    final = lcd_mount_1
+    posts = mount_post(*screw_pos1) \
+      + mount_post(*screw_pos2) \
+      + mount_post(*screw_pos3) \
+      + mount_post(*screw_pos4)
 
-    final = _body() \
-        + lcd_mount_1 \
-        + lcd_mount_2 \
-        + lcd_mount_3 \
-        + lcd_mount_4 \
-        - _lcd_window()
+    screw_holes = screw_hole(*screw_pos1) \
+                    + screw_hole(*screw_pos2) \
+                    + screw_hole(*screw_pos3) \
+                    + screw_hole(*screw_pos4)
+
+    final = base + posts + window() + screw_holes
 
     scad_render_to_file(final, "build/hl3_headwater_faceplate.scad")
 

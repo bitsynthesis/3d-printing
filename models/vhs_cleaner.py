8aca3e0f34e5b6dcfa0c51c3b3200d3ea1126185
Ben Baker-Smith <bbakersmith@gmail.com>
2021-05-01 18:15:13 -0400

    WIP pads

--- models/vhs_cleaner.py
+++ models/vhs_cleaner.py
@@ -46,13 +46,86 @@ def _guide_post(base_diameter):
     return base + foot + neck
 
 
+def _outside_pad(gap_width, total_width, body_height, body_thickness, pad_height):
+    tab_width = 10
+    tab_middle_overhang = 6
+    tab_height = pad_height
+    full_tab_width = tab_width + tab_middle_overhang
+
+    tab_1 = pipe(
+        cube([full_tab_width, 1, tab_height]),
+        translate([
+            ((gap_width / 2) + tab_width) * -1,
+            body_thickness,
+            body_thickness
+        ])
+    )
+
+    tab_2 = pipe(
+        cube([full_tab_width, 1, tab_height]),
+        translate([
+            (gap_width / 2) - tab_middle_overhang,
+            body_thickness,
+            body_thickness
+        ])
+    )
+
+    middle = pipe(
+        cube([gap_width, body_thickness, body_height]),
+        translate([gap_width / -2, 0, 0])
+    )
+
+    return tab_1 + middle + tab_2
+
+
+def _inside_pad(pad_width, body_thickness, tab_width, tab_height, body_height, pad_height):
+    pad = pipe(
+        cube([
+            pad_width,
+            body_thickness,
+            pad_height
+        ]),
+        translate([pad_width / -2, 0, tab_height])
+    )
+
+    bottom_tab_1 = pipe(
+        cube([tab_width, body_thickness, tab_height]),
+        translate([tab_width / -2, 0, 0]),
+        translate([pad_width / 4, 0, 0])
+    )
+
+    bottom_tab_2 = pipe(
+        cube([tab_width, body_thickness, tab_height]),
+        translate([tab_width / -2, 0, 0]),
+        translate([pad_width / -4, 0, 0])
+    )
+
+    top_tab_height = body_height - body_thickness - pad_height
+    top_tab_1 = pipe(
+        cube([tab_width, body_thickness, top_tab_height]),
+        translate([tab_width / -2, 0, 0]),
+        translate([pad_width / 4, 0, body_thickness + pad_height])
+    )
+
+    top_tab_2 = pipe(
+        cube([tab_width, body_thickness, top_tab_height]),
+        translate([tab_width / -2, 0, 0]),
+        translate([pad_width / -4, 0, body_thickness + pad_height])
+    )
+
+    return pad + bottom_tab_1 + bottom_tab_2 + top_tab_1 + top_tab_2
+
+
 def main():
     body_width = 188
     body_depth = 102
     body_thickness = 1.25
     body_height = 25
     # body_height = 5
-    body_height = 12.5
+    # body_height = 12.5
+    bottom_height = body_height / 2
+    top_height = body_height / 2
+    tape_plane_height = 16
 
     spool_hole_diameter = 34
     spool_width = 88.5 + 2 # extra 2 is for amount it can move in the hole
@@ -84,22 +157,30 @@ def main():
     )
 
     front_wall = pipe(
-        cube([body_width, body_thickness, body_height]),
+        cube([body_width, body_thickness, bottom_height]),
         translate([body_width / -2, 0, 0])
     )
 
+    outside_pad_gap_width = 70
+    outside_pad_gap = pipe(
+        cube([outside_pad_gap_width, 10, body_height * 2]),
+        translate([outside_pad_gap_width / -2, -5, body_height / -2])
+    )
+
+    front_wall -= outside_pad_gap
+
     back_wall = pipe(
-        cube([body_width, body_thickness, body_height]),
+        cube([body_width, body_thickness, bottom_height]),
         translate([body_width / -2, body_depth - body_thickness, 0])
     )
 
     side_wall_1 = pipe(
-        cube([body_thickness, body_depth, body_height]),
+        cube([body_thickness, body_depth, bottom_height]),
         translate([body_width / -2, 0, 0])
     )
 
     side_wall_2 = pipe(
-        cube([body_thickness, body_depth, body_height]),
+        cube([body_thickness, body_depth, bottom_height]),
         translate([(body_width / 2) - body_thickness, 0, 0])
     )
 
@@ -139,45 +220,67 @@ def main():
         ])
     )
 
-    cleaning_trough_width = 160
-    cleaning_trough_depth = 6
-    cleaning_trough = pipe(
-        _cleaning_trough(cleaning_trough_width, cleaning_trough_depth, body_height),
-        translate([0, guide_post_from_front + 1 + body_thickness, 0])
+    # cleaning_trough_width = 160
+    # cleaning_trough_depth = 6
+    # cleaning_trough = pipe(
+    #     _cleaning_trough(cleaning_trough_width, cleaning_trough_depth, body_height),
+    #     translate([0, guide_post_from_front + 1 + body_thickness, 0])
+    # )
+    #
+    # support_fill = pipe(
+    #     cube([
+    #         cleaning_trough_width,
+    #         body_depth - cleaning_trough_depth - body_thickness,
+    #         body_height
+    #     ]),
+    #     translate([
+    #         cleaning_trough_width / -2,
+    #         cleaning_trough_depth + body_thickness,
+    #         0
+    #     ])
+    # )
+    #
+    # # hack
+    # support_crop_1 = pipe(
+    #     cube([20, 20, 100]),
+    #     translate([70, 16, 0])
+    # )
+    #
+    # # hack
+    # support_crop_2 = pipe(
+    #     cube([20, 20, 100]),
+    #     translate([-90, 16, 0])
+    # )
+
+    outside_pad = _outside_pad(
+        outside_pad_gap_width,
+        body_width - (body_thickness * 2) - 2,
+        body_height,
+        body_thickness,
+        tape_plane_height
     )
 
-    support_fill = pipe(
-        cube([
-            cleaning_trough_width,
-            body_depth - cleaning_trough_depth - body_thickness,
-            body_height
-        ]),
-        translate([
-            cleaning_trough_width / -2,
-            cleaning_trough_depth + body_thickness,
-            0
-        ])
+    inside_pad_width = outside_pad_gap_width - 12
+    inside_pad_tab_width = 20
+    inside_pad = pipe(
+        _inside_pad(
+            inside_pad_width,
+            body_thickness,
+            inside_pad_tab_width,
+            body_thickness,
+            body_height,
+            tape_plane_height
+        ),
+        translate([0, 6, 0])
     )
 
-    # hack
-    support_crop_1 = pipe(
-        cube([20, 20, 100]),
-        translate([70, 16, 0])
-    )
+    # support_fill = support_fill \
+    #     - dummy_spool_1 \
+    #     - dummy_spool_2 \
+    #     - support_crop_1 \
+    #     - support_crop_2
 
-    # hack
-    support_crop_2 = pipe(
-        cube([20, 20, 100]),
-        translate([-90, 16, 0])
-    )
-
-    support_fill = support_fill \
-        - dummy_spool_1 \
-        - dummy_spool_2 \
-        - support_crop_1 \
-        - support_crop_2
-
-    final = bottom \
+    final_bottom = bottom \
         - spool_hole_1 \
         - spool_hole_2 \
         + front_wall \
@@ -185,9 +288,16 @@ def main():
         + side_wall_1 \
         + side_wall_2 \
         + guide_post_1 \
-        + guide_post_2 \
-        + cleaning_trough \
-        + support_fill
+        + guide_post_2
+
+    final = final_bottom \
+        + dummy_spool_1 \
+        + dummy_spool_2
+
+        # + cleaning_trough \
+        # + support_fill
+
+    final += outside_pad + inside_pad
 
     scad_render_to_file(final, "build/vhs_cleaner.scad")
 

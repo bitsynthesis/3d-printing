6af04af7ef437f01de3c2b34878a354408291cec
Ben Baker-Smith <bbakersmith@gmail.com>
2022-04-26 12:55:55 -0400

    kaptivator tweaks

--- models/kaptivator_bottom.py
+++ models/kaptivator_bottom.py
@@ -2,11 +2,12 @@ from solid import *
 from shared.main import *
 
 
-panel_thickness = 2.5
-panel_thickness = 1.5 # debug
+# panel_thickness = 2.5
+panel_thickness = 2.1
+# panel_thickness = 1.5 # debug
 
 panel_a = 150 - 1
-panel_a = 20 # debug
+# panel_a = 20 # debug
 
 panel_b = 20
 panel_c = 17.5 - 1
@@ -18,6 +19,8 @@ panel_h = 67 - 1
 panel_i = 147 - 1
 panel_j = 226 - 1
 
+panel_width = panel_a * 2
+
 hole_width = 3.5
 hole_height = 5.5
 hole_segments = 32
@@ -27,16 +30,124 @@ access_hole_height = 65
 access_hole_width = 36
 
 
+
+
+
+
+
+
+
+
+
+
+sd_height = 52.75 # actual 52.5
+sd_width = 35.75 # actual 35.5
+sd_center_thickness = 5.5
+sd_side_thickness = 2.5
+sd_top_height = 25
+
+
+sd_total_thickness = 11.5
+sd_total_width = 44.25 # actual 44
+sd_adapter_side_gap_width = 3.5
+sd_adapter_side_gap_thickness = 5
+
+
+thin_border = sd_adapter_side_gap_width
+thick_border = 10
+
+
+hex_diameter = 5.8 # actual 5.25
+hex_height = 6.3 # actual 6
+screw_diameter = 3 # actual 2.5
+
+
+runway_height = 16
+runway_width = 30
+
+
+def _screw_mount():
+    hex_hole = cylinder(h=hex_height, d=hex_diameter, segments=6)
+
+    screw_hole = pipe(
+        cylinder(h=sd_total_thickness + thin_border, d=screw_diameter, segments=100),
+        translate([0, 0, hex_height]),
+    )
+
+    return pipe(hex_hole + screw_hole, hole())
+
+
+def _lid():
+    plateau = pipe(
+        cube([
+            runway_width,
+            sd_top_height,
+            sd_center_thickness
+        ]),
+        translate([
+            thin_border + sd_total_width - runway_width,
+            sd_height + thin_border + runway_height - sd_top_height,
+            0
+        ])
+    )
+
+    cutout = pipe(
+        cube([
+            runway_width,
+            sd_height + runway_height - sd_top_height,
+            panel_thickness * 2
+        ]),
+        translate([
+            thin_border + sd_total_width - runway_width,
+            thin_border,
+            sd_center_thickness
+        ]),
+        hole()
+    )
+
+    return plateau + cutout
+
+
+def _sd_mount_screws():
+    screw = pipe(
+        _screw_mount(),
+        translate([
+            int((thin_border + sd_total_width - sd_width) / 2),
+            int((runway_height + thin_border) / 4),
+            0
+        ])
+    )
+
+    top_screw = pipe(
+        _screw_mount(),
+        translate([
+            int((thin_border + sd_total_width + thin_border) - (thick_border / 2)),
+            thin_border + sd_height + int(thick_border / 2) + runway_height,
+            0
+        ])
+    )
+
+    return screw + top_screw
+
+
+
+
+
+
+
+
+
+
 def _screw_hole(x, y):
     """
     Create an oblong screw hole, with x and y representing the left and top.
     """
-    a = cylinder(h=panel_thickness, d=hole_width, segments=hole_segments)
+    a = cylinder(h=panel_thickness * 2, d=hole_width, segments=hole_segments)
 
     b_center = hole_height - hole_width
 
     b = pipe(
-        cylinder(h=panel_thickness, d=hole_width, segments=hole_segments),
+        cylinder(h=panel_thickness * 2, d=hole_width, segments=hole_segments),
         translate([0, b_center, 0])
     )
 
@@ -49,10 +160,24 @@ def _screw_hole(x, y):
     )
 
 
-def _left_panel():
-    body = cube([panel_a, panel_f, panel_thickness])
+def _debug_cutout():
+    border = 20
 
-    screw_0 = _screw_hole(0, 0)
+    body = cube([
+        (panel_a * 2) - (border * 2),
+        panel_f - (border * 2),
+        panel_thickness * 2,
+    ])
+
+    return pipe(
+        body,
+        translate([border, border, 0]),
+        hole(),
+    )
+
+
+def _left_panel():
+    body = cube([panel_a - right_offset, panel_f, panel_thickness])
 
     screw_1 = _screw_hole(panel_d, panel_g)
     screw_2 = _screw_hole(panel_e, panel_h)
@@ -62,8 +187,40 @@ def _left_panel():
     return body + screw_1 + screw_2 + screw_3 + screw_4
 
 
+# TODO
+right_overlap = 10
+right_offset = 10
+right_width = panel_a + right_overlap + right_offset
+
+
+def _right_panel():
+    body = cube([
+        right_width,
+        panel_f,
+        panel_thickness * 2
+    ])
+
+    body += _screw_hole(right_width - panel_c, panel_g)
+    body += _screw_hole(right_width - panel_e, panel_h)
+    body += _screw_hole(right_width - panel_e, panel_i)
+    body += _screw_hole(right_width - panel_c, panel_j)
+    body += _screw_hole(right_overlap + right_offset, panel_g)
+    body += _screw_hole(right_overlap + right_offset, panel_j)
+
+    sd_mount_elements = pipe(
+        _lid() - pipe(_sd_mount_screws(), translate([0, 0, hex_height * -1])),
+        translate([149, 39, sd_center_thickness * -1])
+    )
+
+    return pipe(
+        body,
+        translate([panel_a - right_overlap - right_offset, 0, 0]),
+        color("purple")
+    ) - _left_panel() +  sd_mount_elements
+
+
 def main():
-    final = _left_panel()
+    final = _left_panel() + _right_panel() # + _debug_cutout()
 
     #  0      1      2
     #
@@ -101,6 +258,20 @@ def main():
     # final = cube([10, 10, 1], center=True) + screw_hole(0, 0)
 
     scad_render_to_file(final, "build/kaptivator_bottom.scad")
+
+    final_left = pipe(
+        _left_panel(),
+        rotate([180, 0, 0]),
+        translate([0, 0, panel_thickness]),
+    )
+    scad_render_to_file(final_left, "build/kaptivator_bottom_left.scad")
+
+    final_right = pipe(
+        _right_panel(),
+        rotate([180, 0, 0]),
+        translate([0, 0, panel_thickness * 2]),
+    )
+    scad_render_to_file(final_right, "build/kaptivator_bottom_right.scad")
 
 
 if __name__ == "__main__":
--- models/kaptivator_sd_mount.py
+++ models/kaptivator_sd_mount.py
@@ -2,15 +2,15 @@ from solid import *
 from shared.main import *
 
 
-sd_height = 52.75 # padded .25
-sd_width = 35.75 # padded .25
+sd_height = 52.75 # actual 52.5
+sd_width = 35.75 # actual 35.5
 sd_center_thickness = 5.5
 sd_side_thickness = 2.5
-sd_top_height = 27
+sd_top_height = 25
 
 
 sd_total_thickness = 11.5
-sd_total_width = 44.25 # padded .25
+sd_total_width = 44.25 # actual 44
 sd_adapter_side_gap_width = 3.5
 sd_adapter_side_gap_thickness = 5
 
@@ -19,11 +19,13 @@ thin_border = sd_adapter_side_gap_width
 thick_border = 10
 
 
-hex_diameter = 5.5
-hex_height = 6
-screw_diameter = 2.5
+hex_diameter = 5.8 # actual 5.25
+hex_height = 6.3 # actual 6
+screw_diameter = 3 # actual 2.5
 
-runway_height = 30
+
+runway_height = 16
+runway_width = 30
 
 
 # TODO
@@ -36,7 +38,7 @@ def _screw_mount():
     hex_hole = cylinder(h=hex_height, d=hex_diameter, segments=6)
 
     screw_hole = pipe(
-        cylinder(h=sd_total_thickness, d=screw_diameter, segments=100),
+        cylinder(h=sd_total_thickness + thin_border, d=screw_diameter, segments=100),
         translate([0, 0, hex_height]),
     )
 
@@ -52,12 +54,12 @@ def _runway():
 
     card_gap = pipe(
         cube([
-            sd_width,
+            runway_width,
             runway_height,
             sd_total_thickness
         ]),
         translate([
-            thin_border + sd_total_width - sd_width,
+            thin_border + sd_total_width - runway_width,
             thin_border,
             thin_border + sd_side_thickness
         ]),
@@ -78,40 +80,98 @@ def _runway():
         hole()
     )
 
-    screw = pipe(
-        _screw_mount(),
+    # pcb_gap = pipe(
+    #     cube([
+    #         thin_border,
+    #         thin_border + runway_height - thick_border,
+    #         thin_border + sd_total_thickness
+    #     ]),
+    #     translate([
+    #         thin_border + sd_total_width,
+    #         0,
+    #         0
+    #     ]),
+    #     hole()
+    # )
+
+    pos_slope = triangular_prism(runway_width, runway_height - thin_border, sd_side_thickness)
+
+    neg_slope = pipe(
+        cube([runway_width, runway_height, sd_side_thickness + thin_border]) - pos_slope,
+        translate([thin_border + sd_total_width - runway_width, thin_border, thin_border]),
+    )
+
+    return body - neg_slope + card_gap + side_gap
+
+
+def _lid():
+    body = pipe(
+        cube([
+            thin_border + sd_total_width + thin_border,
+            thick_border + sd_height + thin_border + runway_height,
+            thin_border
+        ]),
+        translate([0, 0, sd_center_thickness])
+    )
+
+    plateau = pipe(
+        cube([
+            runway_width,
+            sd_top_height,
+            sd_center_thickness
+        ]),
         translate([
-            int((thin_border + sd_total_width - sd_width) / 2),
-            int((runway_height + thin_border) / 4),
+            thin_border + sd_total_width - runway_width,
+            sd_height + thin_border + runway_height - sd_top_height,
             0
         ])
     )
 
-    return pipe(
-        body + card_gap + side_gap + screw,
+    cutout = pipe(
+        cube([
+            runway_width,
+            sd_height + runway_height - sd_top_height,
+            thin_border
+        ]),
         translate([
-            0,
-            runway_height * -1,
-            0
+            thin_border + sd_total_width - runway_width,
+            thin_border,
+            sd_center_thickness
         ])
     )
 
+    return body + plateau - cutout
 
-def main():
+
+def _body():
     body = cube([
         thin_border + sd_total_width + thin_border,
         thick_border + sd_height + thin_border,
         thin_border + sd_total_thickness
     ])
 
-    primary_gap = pipe(
+    retainer_1 = pipe(
         cube([
-            sd_total_width,
-            sd_height,
-            sd_total_thickness
+            sd_total_width - sd_width,
+            thin_border,
+            thin_border + sd_adapter_side_gap_thickness
         ]),
+        translate([
+            0,
+            sd_height - thin_border,
+            0
+        ])
+    )
+
+    retainer_2 = cube([
+        sd_total_width - sd_width,
+        thin_border,
+        thin_border + sd_adapter_side_gap_thickness
+    ])
+
+    primary_gap = pipe(
+        cube([sd_total_width, sd_height, sd_total_thickness]) - retainer_1 - retainer_2,
         translate([thin_border, thin_border, thin_border]),
-        hole()
     )
 
     side_gap = pipe(
@@ -124,8 +184,7 @@ def main():
             0,
             0,
             thin_border + sd_adapter_side_gap_thickness
-        ]),
-        hole()
+        ])
     )
 
     card_gap = pipe(
@@ -134,20 +193,61 @@ def main():
             thin_border + sd_total_width - sd_width,
             0,
             thin_border + sd_side_thickness
-        ]),
-        hole()
+        ])
+    )
+
+    return body - primary_gap - side_gap - card_gap
+
+
+def _sd_mount_screws():
+    screw = pipe(
+        _screw_mount(),
+        translate([
+            int((thin_border + sd_total_width - sd_width) / 2),
+            int((runway_height + thin_border) / 4),
+            0
+        ])
     )
 
     top_screw = pipe(
         _screw_mount(),
         translate([
-            int((thin_border + sd_total_width + thin_border) / 2),
-            thin_border + sd_height + int(thick_border / 2),
+            int((thin_border + sd_total_width + thin_border) - (thick_border / 2)),
+            thin_border + sd_height + int(thick_border / 2) + runway_height,
             0
         ])
     )
 
-    final = body + primary_gap + side_gap + card_gap + top_screw + _runway()
+    return screw + top_screw
+
+
+def _sd_mount():
+    body = pipe(
+        _body(),
+        translate([0, runway_height, 0])
+    )
+
+    return body + _runway() - _sd_mount_screws()
+
+
+def _sd_lid():
+    lid = pipe(
+        _lid(),
+        translate([0, 0, thin_border + sd_center_thickness])
+    )
+
+    return lid - _sd_mount_screws()
+
+
+# def _left_panel():
+#     body = pipe(
+#         cube(
+#     )
+
+
+def main():
+    # final = _sd_mount() + _sd_lid()
+    final = _sd_mount()
 
     scad_render_to_file(final, "build/kaptivator_sd_mount.scad")
 
